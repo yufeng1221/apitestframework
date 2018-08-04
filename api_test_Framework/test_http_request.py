efe6301e3eb16ee7adf763f2058f5227f5a9e442
yufeng1221 <252039429@qq.com>
2018-08-05 03:14:42 +0800

    321321

--- api_test_Framework/test_http_request.py
+++ api_test_Framework/test_http_request.py
@@ -3,6 +3,7 @@ from ddt import ddt,data
 from api_test_Framework.read_config import ReadConfig
 from api_test_Framework.http_requests import HttpRequest
 from api_test_Framework.do_excel import DoExcel
+from api_test_Framework.test_logger import TestLogger
 
 #测试数据
 ip=ReadConfig().read_config('auto.conf','IP','ip')
@@ -11,17 +12,19 @@ case_id_list=ReadConfig().read_config('auto.conf','FLAG','case_id_list')
 test_data=DoExcel('test_api_data.xlsx','test_data','init_data',mode,case_id_list).do_excel()
 
 #日志收集器
+logger = TestLogger('test')
 
 @ddt
 class TestHttpRequest(unittest.TestCase):
     def setUp(self):#执行用例之前的准备工作
-        print("用例开始执行！")
+        logger.info("用例开始执行！")
         self.t=DoExcel('test_api_data.xlsx','test_data','init_data',mode,case_id_list)
         #保存测试结果的实例
 
     @data(*test_data)#拆分数据
     def test_api(self,item):#测试类里面的函数没有传参
         #执行http请求
+
         res=HttpRequest().http_request(ip+item['url'],eval(item['param']),item['method'])
         #保存测试结果
         self.t.write_data(item['case_id']+1,7,str(res))
